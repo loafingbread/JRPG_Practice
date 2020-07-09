b5d73db38ed74075d6b13d8fd36b31314874409f
singhjason75 <singhjason75@yahoo.com>
2020-07-08 21:06:29 -0700

    Started Battlesystem Script
    Began Work on Battlesystem Script

---
+++ Assets/BattleSystem.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+
+public class BattleSystem : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+
+    public Transform playerBattleStation;
+    public Transform enemyBattlestation;
+
+    UnitController PlayerUnit;
+    UnitController EnemyUnit;
+
+    public BattleState state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = BattleState.START;
+        SetupBattle();
+    }
+
+    void SetupBattle()
+    {
+        GameObject PlayerGO = Instantiate(playerPrefab, playerBattleStation);
+        PlayerUnit = PlayerGO.GetComponent<UnitController>();
+
+        GameObject EnemyGO = Instantiate(enemyPrefab, enemyBattlestation);
+        EnemyUnit = EnemyGO.GetComponent<UnitController>();
+
+        
+    }
+
+    
+}
