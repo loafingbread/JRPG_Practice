dbc94205586e3e7ddb2c6a9a8a8fc328063adf91
singhjason75 <singhjason75@yahoo.com>
2020-07-08 20:55:16 -0700

    UI changes
    made several panels + buttons + UnitController Script

---
+++ Assets/UnitController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitController : MonoBehaviour
+{
+    public string unitName;
+    public int unitLevel;
+
+    public int damage;
+
+    public int maxHP;
+    public int currentHP;
+}
