ee3a0f22addbed99afa33dfbe4ce25d58c2fda43
singhjason75 <singhjason75@yahoo.com>
2020-07-14 07:43:51 -0700

    Completed Brackeys Tutorial
    All changes made in tutorial made

--- Assets/BattleSystem.cs
+++ Assets/BattleSystem.cs
@@ -48,10 +48,92 @@ public class BattleSystem : MonoBehaviour
         PlayerTurn();
     }
 
+    IEnumerator PlayerAttack()
+    {
+        //damage enemy
+        bool isDead = EnemyUnit.TakeDamage(PlayerUnit.damage);
+
+        enemyHUD.SetHP(EnemyUnit.currentHP);
+        DialogueText.text = "Direct Hit!";
+
+        yield return new WaitForSeconds(2f);
+
+        //check if enemy is dead
+        if (isDead)
+        {
+            state = BattleState.WON;
+            EndBattle();
+        }
+        else
+        {
+            state = BattleState.ENEMYTURN;
+            StartCoroutine(EnemyTurn());
+        }
+        //change state based on what happened
+    }
+
+    IEnumerator PlayerHeal()
+    {
+        PlayerUnit.Heal(5);
+
+        playerHUD.SetHP(PlayerUnit.currentHP);
+        DialogueText.text = "You feel renewed strenght";
+
+        yield return new WaitForSeconds(2f);
+
+        state = BattleState.ENEMYTURN;
+        StartCoroutine(EnemyTurn());
+    }
+    IEnumerator EnemyTurn()
+    {
+        DialogueText.text = EnemyUnit.unitName + " attacks!";
+        yield return new WaitForSeconds(1f);
+
+        bool isDead = PlayerUnit.TakeDamage(EnemyUnit.damage);
+
+        playerHUD.SetHP(PlayerUnit.currentHP);
+
+        yield return new WaitForSeconds(1f);
+        if(isDead)
+        {
+            state = BattleState.LOST;
+            EndBattle();
+
+        } else
+        {
+            state = BattleState.PLAYERTURN;
+            PlayerTurn();
+        }
+    }
+    void EndBattle()
+    {
+        if(state == BattleState.WON)
+        {
+            DialogueText.text = "You won the battle";
+        } else if (state == BattleState.LOST)
+        {
+            DialogueText.text = "You were defeated.";
+        }
+    }
     void PlayerTurn()
     {
         DialogueText.text = "Choose an action!";
     }
 
-    
+    public void OnAttackButton()
+    {
+        if (state != BattleState.PLAYERTURN)
+            return;
+
+        StartCoroutine(PlayerAttack());
+    }
+
+    public void OnHealButton()
+    {
+        if (state != BattleState.PLAYERTURN)
+            return;
+
+        StartCoroutine(PlayerHeal());
+    }
+
 }
